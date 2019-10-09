f15bd8ffcdc19a94ff5cb3038cea8bcf7c6c7a89
Robert Keller <robert.joseph.keller@gmail.com>
2019-10-09 02:14:37 -0500

    Add files via upload

---
+++ src/app/article/article.model.ts
@@ -0,0 +1,34 @@
+export class Article {
+    title: string;
+    link: string;
+    votes: number;
+
+    //My code below
+    role: string;
+
+
+    constructor(title: string, link: string, role: string, votes?: number) {
+        this.title = title;
+        this.link = link;
+        this.role = role;
+        this.votes = votes || 0;
+        
+    }
+
+    voteUp(): void {
+        this.votes += 1;
+    }
+
+    voteDown(): void {
+        this.votes -= 1;
+    }
+
+    domain(): string {
+        try {
+            const domainAndPath: string = this.link.split('//')[1];
+            return domainAndPath.split('/')[0];
+        } catch (err) {
+            return null;
+        }
+    }
+}
